062e507fd16ab64d75be662eec6d4f9ecb577907
Shashank Bhagavatula <sbhag3@illinois.edu>
2024-06-11 14:59:26 -0400

    creating new dev branch to add my changes

---
+++ FarmNavigator/app/src/main/java/com/example/farmnavigator/StartScreen.java
@@ -0,0 +1,14 @@
+package com.example.farmnavigator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
